d26c7c0891e1fe571b1e3cf3f0c9a308264526c6
centauria <centauria_v_c@163.com>
2019-03-06 16:47:08 +0800

    yeah

--- pyqtgraph_ex1.py
+++ pyqtgraph_ex1.py
@@ -3,6 +3,8 @@ import pyqtgraph as pg
 from pyqtgraph import QtCore
 import numpy as np
 
+SAMPLE_RATE = 44100
+
 app = pg.mkQApp()
 
 win = pg.GraphicsWindow()
@@ -11,21 +13,29 @@ win.resize(800, 300)
 
 p = win.addPlot()
 p.showGrid(x=True, y=True)
-p.setRange(xRange=[0, 200], yRange=[-3, 3])
+p.setRange(xRange=[0, 1024], yRange=[-3, 3])
 
 curve = p.plot(pen='g')
 curve.setData(np.random.randn(200, ))
 
 p2 = win.addPlot()
-p2.setRange(xRange=[0, 200], yRange=[-60, 40])
+p2.showGrid(x=True, y=True)
+p2.setRange(yRange=[-60, 40])
 c2 = p2.plot(pen='y')
+
+f = 10
 
 
 def update():
-	global curve
-	data = np.random.randn(200, )
+	global curve, f
+	# data = 2 * np.random.rand(1024, ) - 1
+	data = np.sin(2 * np.pi * f / SAMPLE_RATE * np.arange(0, 1024))
+	# data = data + 0.25 * np.hstack((data[1:], [0])) - 0.4 * np.hstack((data[2:], [0, 0]))
 	curve.setData(data)
-	c2.setData(10 * np.log(np.abs(np.fft.fft(data * (1 - data))))[:100])
+	spec = np.fft.fft(data * np.hanning(len(data)), 8192) / len(data)
+	spec = spec[:len(spec) // 2]
+	c2.setData(10 * np.log(np.abs(spec)))
+	f += 10
 
 
 timer = QtCore.QTimer()
